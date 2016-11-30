83cb159a49e08da6dc7aee5b8e438da899d4930b
lypeer <luoyanglizi@outlook.com>
2016-11-30 18:33:07 +0800

    2016-11-30

--- liar.sh
+++ liar.sh
@@ -3,23 +3,37 @@
 echo -e "\033[36mStep1: Input the range you want, default is \033[1;32m365:\033[0m"
 read RANGE
 
-if [ "${RANGE}" = "" ]
-then
+if [ "${RANGE}" = "" ] ;then
   RANGE=365
 fi
-
-echo -e ${RANGE}
 
 cat <<EOF >log.txt
   count         date            msg
 EOF
 
-for ((i = 1; i <= ${RANGE}; i++))
-do
-tempDate=`date -v -${i}d +%Y-%m-%d`
+function writeLog() {
 cat <<EOF >>log.txt
-commit ${i}     ${tempDate}     ${tempDate}
+commit ${2}     ${1}     ${1}
 EOF
 git add .
-git commit --date=${tempDate} -am ${tempDate}
+git commit --date=${1} -am ${1}
+}
+
+for ((i = 1; i <= ${RANGE}; i++))
+do
+  tempDate=`date -v -${i}d +%Y-%m-%d`
+  ran=$RANDOM
+  if [ 10000 -ge ${ran} ]; then
+    continue
+  else
+    writeLog ${tempDate} ${i}
+  fi
+
+  if [ 25000 -le ${ran} ]; then
+    writeLog ${tempDate} ${i}
+  fi
+
+  if [ 30000 -le ${ran} ]; then
+    writeLog ${tempDate} ${i}
+  fi
 done
